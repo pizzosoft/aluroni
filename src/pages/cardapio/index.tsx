0a88c90e3ede2631a85f66647c476b41aca577bc
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-25 12:13:03 -0300

    Reat Router Dom e Outlet
    Melhorando toda a estrutura do router

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -5,7 +5,6 @@ import styles from './Cardapio.module.scss';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
-import Menu from 'componentes/Menu';
 export default function Cardapio() {
 
 	const [busca, setBusca] = useState('');
@@ -13,26 +12,19 @@ export default function Cardapio() {
 	const [ordenador, setOrdenador] = useState('');
 
 	return (
-		<main>
-			<Menu />
-			<header className={styles.header}>
-				<div className={styles.header__text}>
-					A casa do código e da massa
-				</div>
-			</header>
-			<section className={styles.cardapio}>
-				<h3 className={styles.cardapio__titulo}>Cardápio</h3>
-				<Buscador
-					busca={busca}
-					setBusca={setBusca} />
-				<div className={styles.cardapio__filtros}>
-					<Filtros
-						filtro={filtro}
-						setFiltro={setFiltro} />
-					<Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-				</div>
-				<Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-			</section>
-		</main>
+		<section className={styles.cardapio}>
+			<h3 className={styles.cardapio__titulo}>Cardápio</h3>
+			<Buscador
+				busca={busca}
+				setBusca={setBusca} />
+			<div className={styles.cardapio__filtros}>
+				<Filtros
+					filtro={filtro}
+					setFiltro={setFiltro} />
+				<Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+			</div>
+			<Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+		</section>
+
 	);
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
+import Menu from 'componentes/Menu'
+import PaginaPadrao from 'componentes/PaginaPadrao'
 import Cardapio from 'pages/cardapio'
 import Inicio from 'pages/inicio'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+
 export default function AppRouter() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Inicio />} />
-				<Route path='/cardapio' element={<Cardapio />} />
-			</Routes>
-		</Router>
+		<main>
+			<Router>
+				< Menu />
+				<Routes>
+					<Route path='/' element={<PaginaPadrao />} >
+						<Route index element={<Inicio />} />
+						<Route path='cardapio' element={<Cardapio />} />
+					</Route>
+				</Routes>
+			</Router>
+		</main>
 	)
 }
