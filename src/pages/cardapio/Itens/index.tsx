15f8774c32f30415ad5140f27b7167792476c1a3
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-25 10:29:23 -0300

    eslint
    Configuração do eslint
    padronização do código
    inicio da construção das rotas

--- src/pages/cardapio/Itens/index.tsx
+++ src/pages/cardapio/Itens/index.tsx
@@ -1,7 +1,7 @@
-import cardapio from './itens.json'
-import Item from './Item'
-import styles from './Itens.module.scss'
-import { useState, useEffect } from 'react'
+import cardapio from './itens.json';
+import Item from './Item';
+import styles from './Itens.module.scss';
+import { useState, useEffect } from 'react';
 
 interface Props {
     busca: string,
@@ -9,43 +9,43 @@ interface Props {
     ordenador: string
 }
 export default function Itens(props: Props) {
-    const [lista, setLista] = useState(cardapio)
-    const { busca, filtro, ordenador } = props
+	const [lista, setLista] = useState(cardapio);
+	const { busca, filtro, ordenador } = props;
 
-    function testaBusca(title: string) {
-        const regex = new RegExp(busca, 'i')
-        return regex.test(title)
-    }
+	function testaBusca(title: string) {
+		const regex = new RegExp(busca, 'i');
+		return regex.test(title);
+	}
 
-    function testaFiltro(id: number) {
-        if (filtro != null) return filtro === id
-        return true
-    }
+	function testaFiltro(id: number) {
+		if (filtro != null) return filtro === id;
+		return true;
+	}
 
-    function ordenar(novalista: typeof cardapio) {
-        switch (ordenador) {
-            case 'porcao':
-                return novalista.sort((a, b) => a.size > b.size ? 1 : -1)
-            case 'qtd_pessoas':
-                return novalista.sort((a, b) => a.serving > b.serving ? 1 : -1)
-            case 'preco':
-                return novalista.sort((a, b) => a.price > b.price ? 1 : -1)
-            default:
-                return novalista
-        }
-    }
-    useEffect(() => {
-        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-        setLista(ordenar(novaLista));
-      },[busca, filtro, ordenador])
+	function ordenar(novalista: typeof cardapio) {
+		switch (ordenador) {
+		case 'porcao':
+			return novalista.sort((a, b) => a.size > b.size ? 1 : -1);
+		case 'qtd_pessoas':
+			return novalista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+		case 'preco':
+			return novalista.sort((a, b) => a.price > b.price ? 1 : -1);
+		default:
+			return novalista;
+		}
+	}
+	useEffect(() => {
+		const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+		setLista(ordenar(novaLista));
+	},[busca, filtro, ordenador]);
 
-    return (
-        <div className={styles.itens}>
-            {lista.map(item => (
-                <Item
-                    key={item.id}
-                    {...item} />
-            ))}
-        </div>
-    )
+	return (
+		<div className={styles.itens}>
+			{lista.map(item => (
+				<Item
+					key={item.id}
+					{...item} />
+			))}
+		</div>
+	);
 }
