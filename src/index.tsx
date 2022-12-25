7b9600788cbd9a5c9b9bc13c4ae6c484fa0675a9
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-25 10:58:56 -0300

    Construção da Tela de Inicio e refactoring
    Refactoring da base do cardapio
    Contrução da tela de inicio
    exibição da recomendação de produtos

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/cardapio';
+import Inicio from 'pages/inicio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<Cardapio />
+		<Inicio/>
 	</React.StrictMode>
 );
